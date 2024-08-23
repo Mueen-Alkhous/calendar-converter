325a8bd1a4946c46ac0a85e7960fd47ca9b18194
Mueen-Alkhous <alkhousmueen3@gmail.com>
2024-08-23 19:09:08 +0300

    Upload Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule, NumberValueAccessor } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import moment from 'moment-hijri';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'calendar-converter';
+export class AppComponent  {
+  
+  title = "convertor"
+
+  gregorianDate: string = new Date().toLocaleDateString()
+  hijriDate: string = ""
+
+  ngOnInit(): void {
+    console.log(this.gregorianDate)
+  }
+
+  gregorianToHijri(){
+    this.hijriDate = moment(this.gregorianDate).format('iYYYY/iM/iD')
+  }
+
 }
+
